1ce1939bc0807f072f90f86b7f4ce2fe0bf6ba1f
Ryan Grieve <ryan@rehabstudio.com>
2013-09-18 14:35:28 +0100

    stubs and typedefs for action mapping

--- src/popo/core/InputManager.h
+++ src/popo/core/InputManager.h
@@ -2,8 +2,16 @@
 #define POPO_INPUTMANAGER_H
 
 #include <iostream>
+#include <stdexcept>
+#include <map>
+#include <functional>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+
+typedef std::function<void(SDL_Event*)> ActionFunc;
+typedef std::map<std::string, ActionFunc> ActionNameMap;
+typedef std::map<int, ActionFunc> EventCodeMap;
+typedef std::map<int, EventCodeMap> EventTypeMap;
 
 namespace popo{
 	namespace core{
@@ -14,10 +22,18 @@ namespace popo{
 class popo::core::InputManager{
 private:
 
+	EventTypeMap _eventTypeMap;
+	ActionNameMap _actionNameMap;
+
 public:
 
 	InputManager();
 	virtual ~InputManager();
+
+	void addListener(int eventType, int code, std::string& action);
+	void addListener(int eventType, int code, ActionFunc callback);
+
+	void addAction(std::string& name, ActionFunc callback);
 
 	void handleKeypress(SDL_Event& event);
 };
