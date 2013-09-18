8ae7c0c32ba5c290b57f3042fa7400947f7a899f
Ryan Grieve <ryan@rehabstudio.com>
2013-09-18 14:35:43 +0100

    implementation of action mapping and acting upon them on keypress

--- src/popo/core/InputManager.cc
+++ src/popo/core/InputManager.cc
@@ -1,22 +1,58 @@
 #include <popo/core/InputManager.h>
 
 popo::core::InputManager::InputManager(){
-	
+	_eventTypeMap = EventTypeMap();
+	_actionNameMap = ActionNameMap();
 };
 
 popo::core::InputManager::~InputManager(){
 	
 };
 
-void popo::core::InputManager::handleKeypress(SDL_Event& event){
-	if(event.key.state == SDL_PRESSED){
-		std::cout << "PRESSED ";
-	}
-	if(event.key.state == SDL_RELEASED){
-		std::cout << "RELEASED ";
+void popo::core::InputManager::addListener(int eventType, int code, std::string& action){
+
+	if(_actionNameMap[action]){
+		addListener(eventType, code, _actionNameMap[action]);
 	}
 
-	std::cout << "SCAN: " << event.key.keysym.scancode << " ";
-	std::cout << "SYM: " << event.key.keysym.sym << " ";
-	std::cout << "MOD: " << event.key.keysym.mod << std::endl;
+};
+
+void popo::core::InputManager::addListener(int eventType, int code, ActionFunc callback){
+
+	try{
+		_eventTypeMap.at(eventType);
+	} catch(const std::out_of_range& err){
+		_eventTypeMap[eventType] = EventCodeMap();
+	};
+
+	_eventTypeMap[eventType][code] = callback;
+
+};
+
+void popo::core::InputManager::addAction(std::string& name, ActionFunc callback){
+
+	_actionNameMap[name] = callback;
+
+}
+
+void popo::core::InputManager::handleKeypress(SDL_Event& event){
+
+	int code;
+
+	if(event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
+	{
+		code = event.key.keysym.sym;
+		std::cout << "SYM: " << code << std::endl;
+	}
+
+	try {
+
+		_eventTypeMap.at(event.type).at(code)(&event);
+
+	} catch(const std::out_of_range& err){
+
+		return;
+
+	}
+
 };
